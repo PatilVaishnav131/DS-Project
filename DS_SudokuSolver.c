61a44ad6aea176b4abd07faa6fc74f4aa682595b
PatilVaishnav131 <vaishnavpatil405@gmail.com>
2025-01-22 19:35:18 +0530

    Added Html Index

--- DS_SudokuSolver.c
+++ DS_SudokuSolver.c
@@ -18,7 +18,7 @@ int isSafe(int grid[N][N], int row, int col, int num) {
         if (grid[row][x] == num || grid[x][col] == num)
             return 0;
     }
-
+//fhf
     int startRow = row - row % 3, startCol = col - col % 3;
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
